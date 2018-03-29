dceaae4bbc23e03cb188091b48d40971a500213c
mrnul <16276083+mrnul@users.noreply.github.com>
2018-03-29 23:36:07 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,24 +3,17 @@
 int main()
 {
 	//XOR data
-	vector<vector<double>> data = { { 1,1 },{ 1,0 },{ 0,1 },{ 0,0 } };
-	vector<vector<double>> targets = { { 0 },{ 1 },{ 1 },{ 0 } };
+	vector<vector<float>> data = { { 0,0 },{ 1,0 },{ 0,1 },{ 1,1 } };
+	vector<vector<float>> targets = { { 0 },{ 1 },{ 1 },{ 0 } };
 
-	//1 input layer with 2 nodes
-	//1 hidden layer with 2 nodes
-	//1 output layer with 1 node
-	vector<unsigned int> topology = { 2 ,2 ,1 };
+	//create a neural network with 3 layers
+	//2 input nodes, 2 hidden nodes, 1 output node
+	NeuralNetwork nn({ 2 , 2 , 1 });
 
-	//create network with random weights in range [-0.5, 0.5]
-	NeuralNetwork nn(topology, -0.5, 0.5);
-	
-	double error = 0;
-	do{
-		nn.Train(data, targets, 1);
-		error = nn.Error(data, targets);
-		cout << error << endl;
-	} while (error > 0.0001);
+	while (true)
+	{
+		nn.Train(data, targets, 0.75);
+		std::cout << nn.SquareError(data, targets) << std::endl;
+	}
 
-	cout << "End" << endl;
-	std::cin.get();
 }
